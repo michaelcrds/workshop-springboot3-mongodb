c822875c8952567329469b25f9d45e7e520e49e6
Michael Cardoso <michael.cardoso1810@gmail.com>
2023-02-21 16:47:07 -0300

    Post title query method

--- src/main/java/com/michaelcardoso/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/michaelcardoso/workshopmongo/repositories/PostRepository.java
@@ -1,10 +1,12 @@
 package com.michaelcardoso.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.michaelcardoso.workshopmongo.entities.Post;
 
 
 public interface PostRepository extends MongoRepository<Post, String> {
-	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
