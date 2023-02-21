30040eef8226eb84678cc7698bd4bc8830f8eec2
Michael Cardoso <michael.cardoso1810@gmail.com>
2023-02-21 15:37:40 -0300

    Post Service and Resource. Find post by Id

---
+++ src/main/java/com/michaelcardoso/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.michaelcardoso.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.michaelcardoso.workshopmongo.entities.Post;
+import com.michaelcardoso.workshopmongo.repositories.PostRepository;
+import com.michaelcardoso.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id(" + id + ")")); 
+	}
+}
